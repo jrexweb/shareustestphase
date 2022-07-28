b34ca0b9e0ca93f71044e0ea05459886900d0429
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-07-28 11:25:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -386,21 +386,23 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
+
     url = f'https://api.shareus.in/shortLink'
     params = {'token': SHORTENER_API,
               'link': link,
               'format': 'json'
               }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-            data = await response.json(content_type='text/html')
-            if data["status"] == "success":
-                return data['shortlink']
-            else:
-                logger.error(f"Error: {data['message']}")
-                return f'https://api. shareus.in/directLink?token={SHORTENER API}&link={link}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json(content_type='text/html')
+                if data["status"] == "success":
+                    return data['shortlink']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://api.shareus.in/directLink?token={SHORTENER_API}&link={link}'
 
-except Exception as e:
-    logger.error(e)
-    return f'https://api.shareus.in/directLink?token={SHORTENER_API}&link={link}'
+     except Exception as e:
+         logger.error(e)
+         return f'https://api.shareus.in/directLink?token={SHORTENER_API}&link={link}'
